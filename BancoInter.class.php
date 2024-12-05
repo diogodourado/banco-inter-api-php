43bcca6402d5b3e67e7035ed3a88cafe4b51ac16
Diogo Dourado <diogo@dourado.net>
2024-12-05 20:02:46 -0300

    Função request para post, get, put e delete

--- BancoInter.class.php
+++ BancoInter.class.php
@@ -22,4 +22,55 @@ class BancoInter
         $this->baseUrl = 'https://cdpj.partners.bancointer.com.br';
         $this->scope = 'boleto-cobranca.read boleto-cobranca.write';
     }
+
+    function request(string $method, string $endpoint, array $data = [], bool $noAuth = false)
+    {
+        if ($noAuth === false)
+            $BearerToken = $this->getBearerToken();
+
+        $ch = curl_init();
+        $url = $this->baseUrl . '/' . ltrim($endpoint, '/');
+        $method = strtoupper($method);
+
+        curl_setopt_array($ch, [
+            CURLOPT_URL => $url,
+            CURLOPT_SSLCERT => $this->certPath,
+            CURLOPT_SSLKEY => $this->keyPath,
+            CURLOPT_RETURNTRANSFER => true,
+        ]);
+
+
+        $headers = $noAuth === true ? array('Content-Type: application/x-www-form-urlencoded') : array('Authorization: Bearer ' . $BearerToken, 'x-conta-corrente: ' . $this->contaCorrente, 'Content-Type: application/json');
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        if ($method === 'POST') {
+            curl_setopt($ch, CURLOPT_POST, true);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $noAuth === true ? http_build_query($data) : json_encode($data));
+        } elseif (in_array($method, ['PUT', 'DELETE'], true)) {
+            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $method === 'DELETE' ? http_build_query($data) : json_encode($data));
+        } elseif ($method === 'GET' && !empty($data)) {
+            $url .= '?' . http_build_query($data);
+            curl_setopt($ch, CURLOPT_URL, $url);
+        }
+
+        $serverResponse = curl_exec($ch);
+
+        if ($serverResponse === false) {
+            $error = curl_error($ch);
+            curl_close($ch);
+            throw new Exception("Erro cURL: $error");
+        }
+
+        curl_close($ch);
+
+        $response = json_decode($serverResponse, true);
+
+        if ($serverResponse != '' && (json_last_error() !== JSON_ERROR_NONE)) {
+            $serverResponseJson = json_encode($serverResponse);
+            throw new Exception("Erro ao decodificar JSON: " . json_last_error_msg() . " - Mensagem recebida:" . $serverResponseJson);
+        }
+
+        return $response;
+    }
 }
